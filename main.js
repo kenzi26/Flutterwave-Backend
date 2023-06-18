e84ac80cdada8ba87b863ce785065086dd3eec43
Mokwenye Kenechukwu Kelan <131318840+kenzi26@users.noreply.github.com>
2023-06-18 16:40:45 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('payForm');
+form.addEventListener("submit", payNow);
+
+function payNow(e) {
+ e.preventDefault();
+ 
+    FlutterwaveCheckout({
+      public_key: "Add Your Public-Key",
+      tx_ref: "Kenechukwu_"+Math.floor((Math.random() * 1000000000) + 1),
+      amount: document.getElementById("amount").value,
+      currency: "USD",
+	  
+      //payment_options: "card,mobilemoney,ussd",
+	  
+      customer: {
+        email: document.getElementById("email").value,
+        phonenumber: document.getElementById("phoneNumber").value,
+        name: document.getElementById("fullName").value,
+      },
+	  
+      callback: (data)=> { // specified callback function
+        //console.log(data);
+		  const reference = data.tx_ref;
+      alert('Payment complete! Reference: ' + reference);
+      },
+	  
+      customizations: {
+        title: "Kenechukwu Mall",
+        description: "FlutterWave Integration in Javascript."
+		
+       // logo: "flutterwave/usecover.gif",
+      },
+    });
+  }
